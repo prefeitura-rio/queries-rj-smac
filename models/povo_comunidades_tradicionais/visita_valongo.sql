6fd187891bd7723f8a5195fa8f637120d016d965
d116626 <d116626@gmail.com>
2023-12-05 16:18:41 -0300

    chore: refactor models

--- models/povo_comunidades_tradicionais/visita_valongo.sql
+++ models/povo_comunidades_tradicionais/visita_valongo.sql
@@ -1,10 +1,7 @@
-CREATE OR REPLACE TABLE `rj-smac.povo_comunidades_tradicionais.visita_valongo` AS
-(
-  SELECT 
-    PARSE_DATE('%Y%m', CAST(periodo AS STRING)) as ano_mes,
-    CAST(ano AS INT64) as ano,
-    CAST(mes AS STRING) as mes,
-    CAST(valor_visitacao AS INT64) as valor_visitacao
-  FROM `rj-smac.povo_comunidades_tradicionais_staging.visita_valongo`
-  ORDER BY 1
-)
+SELECT 
+  PARSE_DATE('%Y%m', CAST(periodo AS STRING)) as ano_mes,
+  CAST(ano AS INT64) as ano,
+  CAST(mes AS STRING) as mes,
+  CAST(valor_visitacao AS INT64) as valor_visitacao
+FROM `rj-smac.povo_comunidades_tradicionais_staging.visita_valongo`
+ORDER BY 1
